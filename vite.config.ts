464a9cf000c90d533fe603d84c6e69eb28090c6d
QandeelAsif <qandeel.asif@gmail.com>
2023-11-20 13:16:57 +0500

    attempt deployment at netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pharmaweb/'  // Set the base URL for your project
+  base: '/'  // Set the base URL for your project
 });
