b60b23f0b8c23e3bb149ba4501c3eae6d660af93
QandeelAsif <qandeel.asif@gmail.com>
2023-11-20 11:22:18 +0500

    Initial commit

---
+++ src/components/particles/DataLists.tsx
@@ -0,0 +1,277 @@
+export const NavLinks = [
+    {
+        name: "Read",
+        url: "/",
+        dropdown:[
+               {
+                name:'Health Informatics',
+                url:'/'},
+                {
+                    name:'Dental Informatics',
+                    url:'/'
+                },
+                {
+                    name:'Nursing Informatics',
+                    url:'/'
+                },
+                {
+                    name:'Primary Care Informativs',
+                    url:'/'
+                },
+                
+        ]
+    },
+    {
+        name: "Watch",
+        url: "/",
+        dropdown: [
+            {
+                name:'dcece',
+                url:'/'
+            },
+            {
+                name:'dcece',
+                url:'/'
+            }
+        ]
+    },
+    {
+        name: "Community",
+        url: "/"
+    },
+    {
+        name: "Join",
+        url: "/"
+    },
+    {
+        name: "Career",
+        url: "/"
+    },
+    // {
+    //     name: "Join",
+    //     url: "/"
+    // },
+    // {
+    //     name: "Join",
+    //     url: "/"
+    // }
+
+];
+
+export const NavButtons = [
+    {
+        name: "Login",
+        url: "/"
+    },
+    {
+        name: "Signup",
+        url: "/"
+    }
+
+];
+
+
+export const HeroTexts = {
+    firstText: "BEST DESITNATIONS AROUND THE WORLD",
+    secondText: "Informatica - Future of Healthcare Through Informatics",
+    thirdText: "Health Informatics: The Intersection of Healthcare, Information Technology, and Innovation. Discover how we leverage data to improve patient care, streamline healthcare services, and pave the way for a healthier tomorrow.",
+    firstButton: "Find out more",
+    secondButton: 'Stream Webinar Now'
+}
+
+
+export const ServiceTexts = {
+    firstText: "CATEGORY",
+    secondText: "We Offer Best Learning Resources",
+    cards: [
+        {
+            firstText: "Watch",
+            secondText: "Watch webinars on demand and view webcast library."
+        },
+        {
+            firstText: "Attend",
+            secondText: "Annual conference, discussion groups, and networking meetups."
+        },
+        {
+            firstText: "Read and Speak",
+            secondText: "Read the latest in Health Informatica and our magazine"
+        },
+        {
+            firstText: "Learn",
+            secondText: "Learn about digital health and apply for fellowship of Health Informatica."
+        }
+    ]
+}
+
+export const TopDestinationTexts = {
+    firstText: "Top Rated",
+    secondText: "Featured Programs",
+    cards: [
+        {
+            country: "AI showcase stage",
+            price: "5.42k",
+            duration: "10 hours session"
+        },
+        {
+            country: "Digital Health Weel, UK",
+            price: "4.2k",
+            duration: "online"
+        },
+        {
+            country: "Digital Health Summit, France",
+            price: "15k",
+            duration: "4 hours session"
+        },
+        {
+            country: "eHealth Webinars",
+            price: "Free",
+            duration: "online"
+        },
+        {
+            country: "e-meds",
+            price: "3.9k",
+            duration: "online"
+        },
+        // {
+        //     country: "Dubai, UAE",
+        //     price: "9.2k",
+        //     duration: "17 days trip"
+        // },
+    ]
+}
+
+export const BookingStepsTexts = {
+    firstText: "Easy and Fast",
+    secondText: "Register for Your Next Health Informatics Event in 3 Simple Steps",
+    listOfSteps: [
+        {
+            text: "Choose from a variety of health informatics conferences and seminars. Learn from experts in the field and engage with the latest research and developments."
+        },
+        {
+            text: "Complete your registration with a secure payment process. We ensure a smooth and hassle-free experience."
+        },
+        {
+            text: "Join us on the specified dates at the chosen venue, ready to immerse yourself in a world of health informatics. Network with professionals, engage in insightful discussions, and expand your knowledge in this rapidly evolving field."
+        }
+    ],
+    cardOne:
+    {
+        name: "e-med facility expansion",
+        date: "14-29 June  |  by Robbin Jobs",
+        people: "24 people attending"
+    },
+    cardTwo: {
+        status: "Ongoing",
+        destination: "online",
+        completion: "40% Completed"
+    }
+
+}
+
+export const TestimonialTexts = {
+    firstText: "TESTIMONIALS",
+    secondText: "What people say about Us.",
+    feedBacks: [
+        {
+            text: "You did a fabulous job. There were no hitches. Thank you again for all your work planning this trip.",
+            person: "Mike taylor",
+            location: "Lahore, Pakistan"
+        },
+        {
+            text: "Jadoo was great with the entire process from planning to updates during the trip. We had 11 people and everything was perfectly executed. We appreciate all of her hard work. It was truly the trip of a lifetime. Thank you!",
+            person: "Gina",
+            location: "Owerri, Nigeria"
+        },
+        {
+            text: "Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful!",
+            person: "Emma",
+            location: "Vancouver, Canada"
+        },
+        {
+            text: "Thank you for your recommendation and putting the trip together. The resort was absolutely beautiful. The infinity pools, the palm trees in the main pool, the infinity pool in out preferred area overlooking the gulf and the golf course were exceptional....",
+            person: "Loveth",
+            location: "Lagos, Nigeria"
+        },
+    ],
+}
+
+export const NewsletterTexts = {
+    firstText: "Subscribe to get information, latest news and other interesting offers about Jadoo.",
+    placeholderText: "Your email",
+    buttonText: "Subscribe"
+}
+
+export const FooterTexts = {
+    underLogoText: "Book your trip in minutes, get full control for much longer.",
+    quickLinks: {
+        caption: "Quick Links",
+        links: [
+            {
+                name: "Destination",
+                url: "/"
+            },
+            {
+                name: "Hotels",
+                url: "/"
+            },
+            {
+                name: "Flights",
+                url: "/"
+            },
+            {
+                name: "Bookings",
+                url: "/"
+            },
+            {
+                name: "Login",
+                url: "/"
+            },
+            {
+                name: "Signup",
+                url: "/"
+            }
+        ]
+    },
+    contacts: {
+        caption: "Contacts",
+        links: [
+            {
+                name: "FAQs",
+                url: "/"
+            },
+            {
+                name: "Help",
+                url: "/"
+            },
+            {
+                name: "Policies",
+                url: "/"
+            },
+            {
+                name: "Terms & Conditions",
+                url: "/"
+            }
+        ]
+    },
+    more: {
+        caption: "More",
+        links: [
+            {
+                name: "Career",
+                url: "/"
+            },
+            {
+                name: "Airlines",
+                url: "/"
+            },
+            {
+                name: "Airline Fees",
+                url: "/"
+            },
+            {
+                name: "Low Fare Tips",
+                url: "/"
+            }
+        ]
+    }
+}
